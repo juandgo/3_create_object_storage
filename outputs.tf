dc5b862f63dbf24619bb325bde19a924d6ca972d
juanda <juandavidgo1997@gmail.com>
2026-09-16 17:53:34 -0500

    Fix syntax in outputs.tf by removing type argument

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,9 @@
 output "s3_bucket_arn" {
-  type        = string
   description = "The ARN of the created S3 bucket."
   value       = aws_s3_bucket.this.arn
 }
 
 output "s3_bucket_name" {
-  type        = string
   description = "The name of the created S3 bucket."
   value       = aws_s3_bucket.this.id
 }
